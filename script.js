7991be4b3a31e999a766a52a705d2eda1c46b393
Nepomuceno <lucasgnepomuceno@gmail.com>
2026-05-29 23:16:38 -0300

    2.0

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ document.addEventListener('keydown', (e) => {
 /* ---- LINKS DE CONTATO ---- */
 // Substitua pelos dados reais da vendedora!
 const WHATSAPP_NUMERO = '5531981111471';
-const INSTAGRAM_PERFIL = 'macrocrochet';
+const INSTAGRAM_PERFIL = 'manu_martins35';
  
 function abrirWhatsApp() {
   const mensagem = encodeURIComponent(
